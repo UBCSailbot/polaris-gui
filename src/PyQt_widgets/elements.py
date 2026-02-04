ccc65d6b2279153c78a832082ceb61283d5ca4ea
digcheese <jeffxue269@gmail.com>
2026-02-04 14:03:55 -0800

    put desired heading_input_group and rudder_input_group into functions

--- src/PyQt_widgets/elements.py
+++ src/PyQt_widgets/elements.py
@@ -6,8 +6,10 @@ from PyQt5.QtWidgets import (
 from PyQt5.QtGui import QPixmap, QFont
 from PyQt5.QtCore import QTimer, Qt
 
+from config import *
 
-
+# constants
+small_spacing = 2
 
 def init_top_bar(self):
     self.logo_label = QLabel()
@@ -27,6 +29,53 @@ def init_top_bar(self):
     top_bar_layout.addWidget(self.status_label)
     top_bar_layout.addStretch()
     return top_bar_layout
+
+def init_checkbox(self):
+    self.manual_steer_checkbox = QCheckBox("Manual Steering")
+    self.manual_steer_checkbox.toggled.connect(self.set_manual_steer)
+    self.keyboard_checkbox = QCheckBox("Keyboard Mode")
+    self.keyboard_checkbox.toggled.connect(self.toggle_keyboard_mode)
+    
+    checkbox_layout = QHBoxLayout()
+    checkbox_layout.addWidget(self.manual_steer_checkbox)
+    checkbox_layout.addWidget(self.keyboard_checkbox)
+
+    return checkbox_layout
+    
+def init_desired_heading_input_group(self):
+    self.desired_heading_input_layout = QVBoxLayout()
+    self.desired_heading_input = QLineEdit()
+    self.desired_heading_button = QPushButton("Set Desired Heading")
+    self.desired_heading_label = QLabel("Heading Angle:")
+    self.desired_heading_label.setStyleSheet(input_label_style)
+    self.desired_heading_input_layout.addWidget(self.desired_heading_label)
+    self.desired_heading_input_layout.addSpacing(small_spacing)
+    self.desired_heading_input_layout.addWidget(self.desired_heading_input)
+    self.desired_heading_input_layout.addSpacing(small_spacing)
+    self.desired_heading_input_layout.addWidget(self.desired_heading_button)
+    self.desired_heading_button.clicked.connect(self.send_desired_heading)
+    self.desired_heading_input_group = QWidget()
+    self.desired_heading_input_group.setLayout(self.desired_heading_input_layout)
+    
+    return self.desired_heading_input_group
+
+def init_rudder_input_group(self):
+    self.rudder_input = QLineEdit()
+    self.rudder_input_layout = QVBoxLayout()
+    self.rudder_button = QPushButton("Send Rudder")
+    self.rudder_input_label = QLabel("Rudder Angle:")
+    self.rudder_input_label.setStyleSheet(input_label_style)
+    self.rudder_input_layout.addWidget(self.rudder_input_label)
+    self.rudder_input_layout.addSpacing(small_spacing)
+    self.rudder_input_layout.addWidget(self.rudder_input)
+    self.rudder_input_layout.addSpacing(small_spacing)
+    self.rudder_input_layout.addWidget(self.rudder_button)
+    self.rudder_button.clicked.connect(self.send_rudder)
+    self.rudder_input_group = QWidget()
+    self.rudder_input_group.setLayout(self.rudder_input_layout)
+    
+    return self.rudder_input_group
+
 
 def init_left_bar(self):
     return 
--- src/Remote_Debugger.py
+++ src/Remote_Debugger.py
@@ -17,6 +17,9 @@ from PyQt5.QtGui import QPixmap, QFont
 from DataObject import *
 from utility import *
 
+from CAN_processes import *
+from PyQt_widgets import *
+
 ### ----------  PyQt5 GUI ---------- ###
 class CANWindow(QWidget):
     def __init__(self, queue, temp_pipe, cmd_queue, response_queue, can_log_queue):
@@ -106,35 +109,11 @@ class CANWindow(QWidget):
             print(f"Error closing log files: {e}")
         event.accept()
 
-    def init_ui(self):
-        # === Top Bar ===
-        self.logo_label = QLabel()
-        pixmap = QPixmap("logo.png")
-        pixmap = pixmap.scaled(40, 40, Qt.KeepAspectRatio, Qt.SmoothTransformation)
-        self.logo_label.setPixmap(pixmap)
-
-        self.temp_label = QLabel("RPI Temp: --")
-        self.status_label = QLabel("DISCONNECTED")
-        self.status_label.setStyleSheet("color: red")
-
-        top_bar_layout = QHBoxLayout()
-        top_bar_layout.addWidget(self.logo_label)
-        top_bar_layout.addSpacing(10)
-        top_bar_layout.addWidget(self.temp_label)
-        top_bar_layout.addSpacing(10)
-        top_bar_layout.addWidget(self.status_label)
-        top_bar_layout.addStretch()
+    def init_ui(self):     
+        top_bar_layout = init_top_bar(self)
+        checkbox_layout = init_checkbox(self)
 
         # === Left Panel ===
-        small_spacing = 2
-        self.manual_steer_checkbox = QCheckBox("Manual Steering")
-        self.manual_steer_checkbox.toggled.connect(self.set_manual_steer)
-        self.keyboard_checkbox = QCheckBox("Keyboard Mode")
-        self.keyboard_checkbox.toggled.connect(self.toggle_keyboard_mode)
-
-        checkbox_layout = QHBoxLayout()
-        checkbox_layout.addWidget(self.manual_steer_checkbox)
-        checkbox_layout.addWidget(self.keyboard_checkbox)
 
         self.instructions1_display = QLabel("For Rudder    (+/- 3 degrees): A / S / D  (Left / Center / Right)")
         self.instructions2_display = QLabel("For Trim Tab (+/- 3 degrees): Q / W / E (Left / Center / Right)")
@@ -142,33 +121,8 @@ class CANWindow(QWidget):
         self.rudder_display = QLabel("Current Set Rudder Angle:  0 degrees")
         self.trimtab_display = QLabel("Current Trim Tab Angle:   0 degrees")
 
-        self.desired_heading_input_layout = QVBoxLayout()
-        self.desired_heading_input = QLineEdit()
-        self.desired_heading_button = QPushButton("Set Desired Heading")
-        self.desired_heading_label = QLabel("Heading Angle:")
-        self.desired_heading_label.setStyleSheet(input_label_style)
-        self.desired_heading_input_layout.addWidget(self.desired_heading_label)
-        self.desired_heading_input_layout.addSpacing(small_spacing)
-        self.desired_heading_input_layout.addWidget(self.desired_heading_input)
-        self.desired_heading_input_layout.addSpacing(small_spacing)
-        self.desired_heading_input_layout.addWidget(self.desired_heading_button)
-        self.desired_heading_button.clicked.connect(self.send_desired_heading)
-        self.desired_heading_input_group = QWidget()
-        self.desired_heading_input_group.setLayout(self.desired_heading_input_layout)
-
-        self.rudder_input_layout = QVBoxLayout()
-        self.rudder_input = QLineEdit()
-        self.rudder_button = QPushButton("Send Rudder")
-        self.rudder_input_label = QLabel("Rudder Angle:")
-        self.rudder_input_label.setStyleSheet(input_label_style)
-        self.rudder_input_layout.addWidget(self.rudder_input_label)
-        self.rudder_input_layout.addSpacing(small_spacing)
-        self.rudder_input_layout.addWidget(self.rudder_input)
-        self.rudder_input_layout.addSpacing(small_spacing)
-        self.rudder_input_layout.addWidget(self.rudder_button)
-        self.rudder_button.clicked.connect(self.send_rudder)
-        self.rudder_input_group = QWidget()
-        self.rudder_input_group.setLayout(self.rudder_input_layout)
+        self.desired_heading_input_group = init_desired_heading_input_group(self)
+        self.rudder_input_group = init_rudder_input_group(self)
         
 
         self.trim_input = QLineEdit()
