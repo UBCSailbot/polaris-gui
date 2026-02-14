9c0067f8de1a967f8f5537ad6f4f447a4259a93b
Breanne Chan <bchan101@students.cs.ubc.ca>
2026-02-14 11:18:37 -0800

    Moved test files into their own folder

--- src/test_files/test.py
+++ src/test_files/test.py
@@ -1,5 +1,6 @@
+# ===== Example AIS Graph (Basic) ===== 
 # ===== Testing ScatterPlot =====
-from random import randint
+from random import randint, uniform
 
 import pyqtgraph as pg
 from PyQt5 import QtCore, QtWidgets, QtGui
@@ -9,17 +10,15 @@ class MainWindow(QtWidgets.QMainWindow):
         super().__init__()
 
         self.time = list(range(10)) # create a list of times from 1-10 for time
-        y_data = [2, 3, 3, 2, 1, 7, 9, 9, 9, 6] # randint(1, 9) for i in range(0, 10)]
-        print(y_data)
+        x_data = [uniform(-180, 180) for i in range(10)] 
+        y_data = [uniform(-90, 90) for j in range(10)] # randint(1, 9) for i in range(0, 10)]
+        print("longitude: ", x_data)
+        print("latitude: ", y_data)
 
         polaris_pen = pg.mkPen(color='r', width=5) # set point border color (red)
         polaris_brush = pg.mkBrush(color='r')
         other_pen = pg.mkPen(color='b', width=1)
         other_brush = pg.mkBrush(color='b')
-
-        # Arrow - note that it is kinda pointing wrong; the arrow points to the point, not away from the point
-        vector = pg.makeArrowPath(headLen=0.07, headWidth=0.04, tailLen=0.2, tailWidth=0.01)
-
 
         '''
         Optional list of dicts. Each dict specifies parameters for a single spot: {‘pos’: (x,y), ‘size’, ‘pen’, ‘brush’, ‘symbol’}. 
@@ -28,46 +27,134 @@ class MainWindow(QtWidgets.QMainWindow):
         spots = []
         for pt in self.time:
             if (pt == 5):
-                spots.append({'pos': (pt - 2, y_data[pt - 1]), 'size':300, 'pen': polaris_pen, 'brush': polaris_brush, 'symbol': vector}) # Each point can have its own brush, pen, symbol
+                spots.append({'pos': (x_data[pt - 1], y_data[pt - 1]), 'size':10, 'pen': polaris_pen, 'brush': polaris_brush, 'symbol': 'x'}) # Each point can have its own brush, pen, symbol
             else:
-                spots.append({'pos': ((pt % 5), y_data[pt - 1]), 'size':8, 'pen': other_pen, 'brush': other_brush, 'symbol': 'o'})
+                spots.append({'pos': (x_data[pt - 1], y_data[pt - 1]), 'size':10, 'pen': other_pen, 'brush': other_brush, 'symbol': 'o'})
 
-        # Graph
+        # Graph using PlotWidget
         self.plot_widget = pg.PlotWidget()
-        self.plot_widget.getPlotItem().getViewBox().setMouseEnabled(False, False) # disable graph interaction
-        self.plot_widget.setBackground("w") # set background color to white
-        self.plot_widget.setTitle("Test Data vs Time", color="b", size="20pt") # set title of graph
-        styles = {"color": "red", "font-size": "18px"} # create a style sheet
-        self.plot_widget.setLabel("left", "Temperature (°C)", **styles) # create y-axis label, set its style
-        self.plot_widget.setLabel("bottom", "Time (min)", **styles) # create x-axis label, set its style
-        self.plot_widget.addLegend() # must be called before calling plot to add legend to graph
-        self.plot_widget.showGrid(x=True, y=True) # set grid on graph
-        self.plot_widget.setYRange(0, 10) # fix y-range on graph
 
+        # setup - all from create_graph function
+        self.plot_widget.setBackground("w")
+        self.plot_widget.getPlotItem().getViewBox().setMouseEnabled(False, False)
+        self.plot_widget.setTitle("Ship Positions", color='black')
+        self.plot_widget.setLabel("left", "Latitude")
+        self.plot_widget.setLabel("bottom", "Longitude")
+        self.plot_widget.addLegend()
+        self.plot_widget.showGrid(x=True, y=True)
 
-        self.plot_graph = pg.ScatterPlotItem(spots) # create ScatterPlot object
-        self.plot_widget.addItem(self.plot_graph)
+        # create line objects for polaris & for other ships; add data
+        # TODO: copy necessary bits from create_line here
+        other_line = self.plot_widget.plot(
+            x_data, 
+            y_data,
+            name="other ships",
+            pen=None,
+            symbolBrush=other_brush,
+            symbol = "o"
+        )
+
+        polaris_line = self.plot_widget.plot(
+            name="Polaris",
+            pen=None,
+            symbolBrush=polaris_brush,
+            symbol="x"
+        )
+
+        polaris_line.setData([0], [0])
+
+        
+        # Graph using ScatterPLotItem
+        # self.plot_widget = pg.PlotWidget()
+        # self.plot_widget.getPlotItem().getViewBox().setMouseEnabled(False, False) # disable graph interaction
+        # self.plot_widget.setBackground("w") # set background color to white
+        # self.plot_widget.setTitle("Longitude vs. Latitude", color="b", size="20pt") # set title of graph
+        # styles = {"color": "red", "font-size": "18px"} # create a style sheet
+        # self.plot_widget.setLabel("left", "Latitude (DD)", **styles) # create y-axis label, set its style
+        # self.plot_widget.setLabel("bottom", "Longitude (DD)", **styles) # create x-axis label, set its style
+        # self.plot_widget.addLegend() # must be called before calling plot to add legend to graph
+        # self.plot_widget.showGrid(x=True, y=True) # set grid on graph
+
+        # self.plot_graph = pg.ScatterPlotItem(spots) # create ScatterPlot object
+        # self.plot_widget.addItem(self.plot_graph)
         
         self.setCentralWidget(self.plot_widget) 
-        
-        # Add a timer to simulate new temperature measurements
-        self.timer = QtCore.QTimer() # timer object
-        self.timer.setInterval(300) # set timer to timeout every 300 milliseconds
-        self.timer.timeout.connect(self.update_plot) # call update plot on timeout
-        self.timer.start() # start timer
-
-    def update_plot(self):
-        pass
-        # self.time = self.time[1:] # remove first element of self.time
-        # self.time.append(self.time[-1] + 1) # add new time to the end of self.time
-        # self.temperature = self.temperature[1:] # remove first element of self.temperature
-        # self.temperature.append(randint(20, 40)) # add new random data point
-        # self.line.setData(self.time, self.y_data) # graph the new data
 
 app = QtWidgets.QApplication([])
 main = MainWindow()
 main.show()
 app.exec()
+
+# # ===== Testing ScatterPlot =====
+# from random import randint
+
+# import pyqtgraph as pg
+# from PyQt5 import QtCore, QtWidgets, QtGui
+
+# class MainWindow(QtWidgets.QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+
+#         self.time = list(range(10)) # create a list of times from 1-10 for time
+#         y_data = [2, 3, 3, 2, 1, 7, 9, 9, 9, 6] # randint(1, 9) for i in range(0, 10)]
+#         print(y_data)
+
+#         polaris_pen = pg.mkPen(color='r', width=5) # set point border color (red)
+#         polaris_brush = pg.mkBrush(color='r')
+#         other_pen = pg.mkPen(color='b', width=1)
+#         other_brush = pg.mkBrush(color='b')
+
+#         # Arrow - note that it is kinda pointing wrong; the arrow points to the point, not away from the point
+#         vector = pg.makeArrowPath(headLen=0.07, headWidth=0.04, tailLen=0.2, tailWidth=0.01)
+
+
+#         '''
+#         Optional list of dicts. Each dict specifies parameters for a single spot: {‘pos’: (x,y), ‘size’, ‘pen’, ‘brush’, ‘symbol’}. 
+#         This is just an alternate method of passing in data for the corresponding arguments.
+#         '''
+#         spots = []
+#         for pt in self.time:
+#             if (pt == 5):
+#                 spots.append({'pos': (pt - 2, y_data[pt - 1]), 'size':300, 'pen': polaris_pen, 'brush': polaris_brush, 'symbol': vector}) # Each point can have its own brush, pen, symbol
+#             else:
+#                 spots.append({'pos': ((pt % 5), y_data[pt - 1]), 'size':8, 'pen': other_pen, 'brush': other_brush, 'symbol': 'o'})
+
+#         # Graph
+#         self.plot_widget = pg.PlotWidget()
+#         self.plot_widget.getPlotItem().getViewBox().setMouseEnabled(False, False) # disable graph interaction
+#         self.plot_widget.setBackground("w") # set background color to white
+#         self.plot_widget.setTitle("Test Data vs Time", color="b", size="20pt") # set title of graph
+#         styles = {"color": "red", "font-size": "18px"} # create a style sheet
+#         self.plot_widget.setLabel("left", "Temperature (°C)", **styles) # create y-axis label, set its style
+#         self.plot_widget.setLabel("bottom", "Time (min)", **styles) # create x-axis label, set its style
+#         self.plot_widget.addLegend() # must be called before calling plot to add legend to graph
+#         self.plot_widget.showGrid(x=True, y=True) # set grid on graph
+#         self.plot_widget.setYRange(0, 10) # fix y-range on graph
+
+
+#         self.plot_graph = pg.ScatterPlotItem(spots) # create ScatterPlot object
+#         self.plot_widget.addItem(self.plot_graph)
+        
+#         self.setCentralWidget(self.plot_widget) 
+        
+#         # Add a timer to simulate new temperature measurements
+#         self.timer = QtCore.QTimer() # timer object
+#         self.timer.setInterval(300) # set timer to timeout every 300 milliseconds
+#         self.timer.timeout.connect(self.update_plot) # call update plot on timeout
+#         self.timer.start() # start timer
+
+#     def update_plot(self):
+#         pass
+#         # self.time = self.time[1:] # remove first element of self.time
+#         # self.time.append(self.time[-1] + 1) # add new time to the end of self.time
+#         # self.temperature = self.temperature[1:] # remove first element of self.temperature
+#         # self.temperature.append(randint(20, 40)) # add new random data point
+#         # self.line.setData(self.time, self.y_data) # graph the new data
+
+# app = QtWidgets.QApplication([])
+# main = MainWindow()
+# main.show()
+# app.exec()
 
 
 # ====== Testing Dropdowns (QComboBox) =======
