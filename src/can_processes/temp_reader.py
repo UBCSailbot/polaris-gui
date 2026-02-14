5e324245c49e439582872fa4222e8bb195d52269
Breanne Chan <bchan101@students.cs.ubc.ca>
2026-02-14 11:35:18 -0800

    Added separate file for temperature_reader process

---
+++ src/can_processes/temp_reader.py
@@ -0,0 +1,29 @@
+import multiprocessing
+import time
+import paramiko
+from data_object import *
+from utility import *
+
+def temperature_reader(pipe):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        client.connect(hostname, username=username, password=password)
+        while True:
+            try:
+                stdin, stdout, stderr = client.exec_command("cat /sys/class/thermal/thermal_zone0/temp")
+                raw = stdout.read().decode().strip()
+                if raw:
+                    temp = float(raw) / 1000
+                    pipe.send((True, f"{temp:.1f}°C"))
+                else:
+                    pipe.send((False, "ERROR"))
+            except Exception:
+                pipe.send((False, "ERROR"))
+            time.sleep(1)
+    except Exception:
+        while True:
+            pipe.send((False, "DISCONNECTED"))
+            time.sleep(1)
+    finally:
+        client.close()
