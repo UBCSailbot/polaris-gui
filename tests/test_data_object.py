3c6e71764c826ab4857581858323696599aa9773
Breanne Chan <bchan101@students.cs.ubc.ca>
2026-02-17 14:46:29 -0800

    Added test for graph visibility

--- tests/test_data_object.py
+++ tests/test_data_object.py
@@ -45,9 +45,9 @@ def test_create_label(arguments, expected_result, qtbot):
 # TODO: test GraphObject class - test all functions
 
 graph_object_params = [
-    ({"x_name": "X data", "y_name": "Y data", "x_units": "sec", "y_units": "cm", "minn": 0, "maxn": 100, "dropdown_label": None}, Outcome.SUCCESS),
-    ({"x_name": "bob", "y_name": "jim", "x_units": "m", "y_units": "bacteria", "minn": 30, "maxn": 80, "dropdown_label": "special_label"}, Outcome.SUCCESS),
-    ({"x_name": "test test", "y_name": "234093rew98", "x_units": "hours", "y_units": "mg/cm^3", "minn": -50, "maxn": -10, "dropdown_label": None}, Outcome.SUCCESS),
+    ({"x_name": "X data", "y_name": "Y data", "x_units": "sec", "y_units": "cm", "minn": 0, "maxn": 100, "dropdown_label": None}),
+    ({"x_name": "bob", "y_name": "jim", "x_units": "m", "y_units": "bacteria", "minn": 30, "maxn": 80, "dropdown_label": "special_label"}),
+    ({"x_name": "test test", "y_name": "234093rew98", "x_units": "hours", "y_units": "mg/cm^3", "minn": -50, "maxn": -10, "dropdown_label": None}),
 ]
 
 @pytest.fixture()
@@ -57,10 +57,10 @@ def init_obj(params, qtbot):
     qtbot.addWidget(obj.graph) # add graph to visible application window
     return obj
 
-@pytest.mark.parametrize("params, expected_outcome", graph_object_params, scope="class")
+@pytest.mark.parametrize("params", graph_object_params, scope="class")
 class Test_GraphObject:
 
-    def test_constructor(self, init_obj, params, expected_outcome):
+    def test_constructor(self, init_obj, params): 
         obj = init_obj
 
         assert obj.x_name == params["x_name"]
@@ -77,7 +77,44 @@ class Test_GraphObject:
         else:
             assert obj.dropdown_label == params["dropdown_label"]
         
-        assert expected_outcome == Outcome.SUCCESS
+        # TODO: add expected_outcome as a parameter to this test, and add bad parameters for creating graphobjects (eg. min > max)
+        # assert expected_outcome == Outcome.SUCCESS
+
+    # def test_update_xlim(self, init_obj, params):
+    #     # TODO: finish implementation
+    #     # NOTE: it would be nice to also take begin, end as parameters to the test
+    #     pass
+
+    def test_visibility(self, init_obj):
+        # hide even after already hidden; make visible after already visible
+        # also obviously change back and forth between states
+        # hide/show multiple times - each time should be correct
+
+        obj = init_obj
+
+        # Setup check
+        assert obj.visible == False
+        assert obj.graph.isVisible() == False 
+
+        # hide when already hidden
+        obj.hide()
+        assert obj.visible == False
+        assert obj.graph.isVisible() == False 
+
+        obj.show()
+        assert obj.visible == True
+        assert obj.graph.isVisible() == True
+
+        # show when already visible
+        obj.show()
+        assert obj.visible == True
+        assert obj.graph.isVisible() == True 
+
+        obj.hide()
+        obj.show()
+        assert obj.visible == True
+        assert obj.graph.isVisible() == True
+        
 
 
 data_object_params = [
