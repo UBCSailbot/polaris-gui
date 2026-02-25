1079cccbd4f58bf3838bb98c48f23b1bb654e6a7
Breanne Chan <bchan101@students.cs.ubc.ca>
2026-02-25 13:38:39 -0800

    Added update_status, is_alive functions to handle internal display update

--- src/project/pyqt_widgets/heartbeat_module.py
+++ src/project/pyqt_widgets/heartbeat_module.py
@@ -11,6 +11,7 @@ class HeartbeatModule():
         self.title_text = title_text
         self.label = None
         self.time_of_last = None # time of last received heartbeat frame
+        self.alive_status = False # Currently displayed connection status
 
     def init_label(self):
         self.label = create_hb_label(self.title_text + f"<font style=\"{cg.heartbeat_status_bad_style}\">{cg.heartbeat_status_bad_text}</font>", 
@@ -21,10 +22,24 @@ class HeartbeatModule():
 
     def initialized(self) -> bool:
         return self.label is not None and self.time_of_last is not None
+            
+    def is_alive(self, current_time) -> bool:
+        if (not self.initialized()):
+            print("ERROR - Module not initialized!")
+            print("self.label = ", self.label, "  |  self.time_of_last = ", self.time_of_last)
+            return False
+        return current_time - self.time_of_last < cg.heartbeat_timeout
+
+    def update_status(self, current_time): 
+        '''Note: only checks for disconnection'''
+        if (not self.is_alive(current_time) and self.alive_status is True):
+            self.set_not_responding()
 
     def set_not_responding(self):
         self.label.setText(self.title_text + f"<font style=\"{cg.heartbeat_status_bad_style}\">{cg.heartbeat_status_bad_text}</font>")
+        self.alive_status = False
 
     def set_alive(self, time):
         self.label.setText(self.title_text + f"<font style=\"{cg.heartbeat_status_good_style}\">{cg.heartbeat_status_good_text}</font>")
+        self.alive_status = True
         self.time_of_last = time
