567f75d106db0213576ac04956c2e71a6e533f80
Breanne Chan <bchan101@students.cs.ubc.ca>
2026-03-13 10:06:39 -0700

    Added is_latched function

--- src/project/joystick_mixin.py
+++ src/project/joystick_mixin.py
@@ -1,6 +1,8 @@
 import pygame
 
-from project.config import movement_sensitivity, num_axes
+from project.config import (
+    movement_sensitivity, num_axes, LATCHED, UNLATCHED
+)
 
 # Handles joystick interaction & function
 # Note: Currently corresponds to maximum one joystick
@@ -35,10 +37,10 @@ class JoystickMixin():
             return self.js_prev_pos[axis]
         return 0xffffffff
 
-    def joystick_moved(self, axis: int) -> tuple[bool, float]:
+    def joystick_moved(self, axis: int, latch_axis) -> tuple[bool, float]:
         '''Returns True and joystick's current position if joystick is connected AND is in a different position from
         when this function was last called, else returns False and 0; updates self.js_prev_pos'''
-        if self.joystick is not None:
+        if self.joystick is not None and not self.is_latched(latch_axis):
             pygame.event.pump()
             pos = round(self.joystick.get_axis(axis), movement_sensitivity)
             if (pos != self.js_prev_pos[axis]):
@@ -47,6 +49,12 @@ class JoystickMixin():
             else:
                 self.js_prev_pos[axis] = pos
         return [False, 0]
+    
+    def is_latched(self, axis) -> bool:
+        if self.joystick is not None:
+            pygame.event.pump() # update position
+            return int(self.joystick.get_axis(axis)) == LATCHED
+        else: return True # default state is latched (locked)
 
     def set_joystick_enabled(self, state: bool) -> None:
         '''Set enabled to state'''
