c6412442de2cc0a2655f946db3d1fc8a6262ed83
Breanne Chan <bchan101@students.cs.ubc.ca>
2026-03-14 16:48:31 -0700

    Fixed canup.sh script

--- src/canup.sh
+++ src/canup.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 # Script to make setting can up/down easier
 
-sudo ip link set can0 down
-echo "can0 set down!"
+if [ "$#" -eq 0 ]; then
+        sudo ip link set can0 down
+        echo "can0 set down!"
+        sudo ip link set can0 up type can bitrate 500000 dbitrate 1000000 fd on loopback off
+        echo "can0 set up with loopback off!"
+fi
 
-while getopts "l:" opt; do
+while getopts "l" opt; do
     case "$opt" in
     l)
-        sudo ip link set can0 up type can bitrate 500000 dbitrate 1000000 fd on
-        echo "can0 set up with loopback off!"
-        ;;
-    :)
+        sudo ip link set can0 down
+        echo "can0 set down!"
         sudo ip link set can0 up type can bitrate 500000 dbitrate 1000000 fd on loopback on
         echo "can0 set up with loopback on!"
         ;;
+    ?)
+        echo "usage: bash canup.sh [-l]"
+        ;;
     esac
 done
+
+shift $((OPTIND - 1))
+
+if [ "$#" -gt 0 ]; then 
+    echo "usage: bash canup.sh [-l]"
+fi
