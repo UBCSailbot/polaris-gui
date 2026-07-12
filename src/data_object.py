e2d1549a21e6930664b20ca979e3b7e13ecd3315
Akshanjay Kompelli <63443017+FireBoyAJ24@users.noreply.github.com>
2026-07-12 09:45:31 -0700

    Fix Visualizer Errors (#35)

--- src/data_object.py
+++ src/data_object.py
@@ -902,8 +902,8 @@ class Docker_Command:
         command_type: Docker_Command_Type,
         config_file="",
         launch_mode="",
-        mock_ais="",  # boolean
-        visualizer_mode="",  # boolean
+        mock_ais=False,
+        visualizer_mode=False,
     ):
 
         self.command_type = command_type
@@ -915,17 +915,15 @@ class Docker_Command:
         self.command = command_type.value
 
         if command_type == Docker_Command_Type.START_CUSTOM:
-            header = (
-                """ros2 launch global_launch main.py record:=true log_level:=debug"""
-            )
+            header = """ros2 launch global_launch main_launch.py record:=true log_level:=debug"""
             footer = """2>&1 | tee src/global_launch/voyage_log/combined_log_$( date +%F_%T).txt"""
             self.command = " ".join(
                 [
                     header,
                     f"config:={config_file}",
                     f"mode:={launch_mode}",
-                    f"on_water_mock_ais:={mock_ais}",
-                    f"visualizer_mode:={visualizer_mode}",
+                    f"on_water_mock_ais:={str(mock_ais).lower()}",
+                    f"visualizer_mode:={str(visualizer_mode).lower()}",
                     footer,
                 ]
             )
